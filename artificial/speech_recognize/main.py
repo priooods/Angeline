0442fb750e5a483df60db4b1ba16e81d383ba84e
priooods <priodwisembodo23@gmail.com>
2021-12-15 03:49:42 +0700

    Artifial Intelegence Development Start

--- artificial/speech_recognize/main.py
+++
@@ -1,7 +0,0 @@
-import librosa
-import librosa.display
-import IPython.display as ipd
-
-
-data,rate = librosa.load(r"C:\Users\prio\Desktop\Angeline\artificial\audio\prio_217.wav")
-librosa.display.waveplot(data,sr=rate)
