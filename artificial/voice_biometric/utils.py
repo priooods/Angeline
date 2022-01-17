68608d16a74d689cb2e7e95351951fab0cd9ac24
priooods <priodwisembodo23@gmail.com>
2022-01-17 16:04:05 +0700

    PRIO:[Utils] update method info

--- artificial/voice_biometric/utils.py
+++ artificial/voice_biometric/utils.py
@@ -3,6 +3,18 @@ import wave,struct
 import pyaudio
 
 def read_audio_file(_file = "",_print_log=False):
+    '''
+    Membaca audio file
+    
+    Parameter
+    ---------
+    _file : File audio yang anda miliki, Required format `.wav`
+    _print_log : check apabila file audio berhasil terbaca. value default `False`
+    
+    Example
+    -------
+    >>> audios = read_audio_file(f'C://../../arificial/audio/prio_1.wav')
+    '''
     wav = wave.open(f"{_file}",'r') # pertama kita membaca file audio yang di jadikan input
     
     if _print_log: # nyalakan true kalau kita mau ngechek apakah method read_audio_file berhasil baca audio input nya
@@ -17,6 +29,24 @@ def read_audio_file(_file = "",_print_log=False):
 
 def save_audio_file(_new_filename = "", _sample_rate = 44100, _n_channels = 1
                     , _sample_width = 1, _sound = None, _print_log=False):
+    '''
+    Menyimpan audio baru dalam bentuk file audio.wav
+    fungsi ini dapat dikombinasikan dengan fungsi looping apabila dibutuhkan
+    
+    Parameter
+    ---------
+    _new_filename : filename audio yang ingin anda tentukan, Required format `String Kosong`
+    _sample_rate : besaran sample rate default. default `44100`
+    _n_channels : opsi channel default. default `1`
+    _sample_width : besaran sample width default. default `1`
+    _sound : masukan audio dari file / live speaker. default `None`
+    _print_log : check apabila audio berhasil disimpan dengan filename yang sudah ditentukan. default `False`
+    
+    Example
+    -------
+    >>> new_file = save_audio_file(audio,_sample_rate,_channel,_sample_width,_sound)
+    '''
+    
     wav = wave.open(_new_filename, "w") # kita buat sebuah file audio dengan ( filename = nama file yang kita buat , w = perintah write )
     wav.setnchannels(_n_channels) # kita atur nilai channel nya ( default = 1 )
     wav.setsampwidth(_sample_width) # kita atur nilai width nya ( default = 1)
@@ -28,8 +58,21 @@ def save_audio_file(_new_filename = "", _sample_rate = 44100, _n_channels = 1
     for i in range(0, len(_sound)): # setelah itu kita susun frame-frame audio menjadi kesatuan 
         wav.writeframesraw(struct.pack('<h', _sound[i]))
     
-    
 def manipulate_audio_filename(_directory = None, _new_filename = "", _remove_file=False):
+    '''
+    Duplicate audio file yang sudah ada dan mengubah filename nya
+    fungsi ini dapat dikombinasikan dengan fungsi looping apabila dibutuhkan
+    
+    Parameter
+    ---------
+    _new_filename : filename baru yang ingin anda tentukan, Required format `String Kosong`
+    _directory : lokasi directory audio lama yang ingin dibaca untuk di ubah filenamenya. default `None`
+    _remove_file : apabila file audio sebelumnya yang telah di duplicate ingin di apus. default `False`
+    
+    Example
+    -------
+    >>> new_file = manipulate_audio_filename(f"_directory","nama file baru",True)
+    '''
     for i,file in enumerate(os.listdir(_directory), start=1): # kita panggil semua file audio yang ada pada sebuah directry folder audio kita
         # lalu kita bedah semua audio yang ada pada directory audio
         _sample_rate, _channel, _sample_width, _sound = read_audio_file(f"{_directory}\{file}",True) # kita keluarkan nilai nilai acuan dari audio yg kita baca
@@ -39,8 +82,25 @@ def manipulate_audio_filename(_directory = None, _new_filename = "", _remove_fil
         if _remove_file: # kita juga bisa menghapus audio lama supaya tidak double ( audio.wav ) apabila dibutuhkan dengan mengubah nya jadi TRUE
             os.remove(f"{_directory}\{file}")
             
-            
-def create_voice_model(path_destination,_model_number=0,_format=pyaudio.paInt16,_channel=2,_rate=44100,_chunk=1024,_durasi_record=4):
+def create_voice_model(path_destination,_model_number=0,_format=pyaudio.paInt16,_channel=1,_rate=44100,_chunk=1024,_durasi_record=4):
+    '''
+    Melakukan recording audio baru secara langsung
+    
+    Parameter
+    ---------
+    path_destination : Lokasi `Path` folder yang akan menyimpan file hasil record 
+    _model_number : Prefix filename , default `0`
+    _format : fornamt filename , default `.wav`
+    _channel : channel yang dipakai , default `1`
+    _rate : nilai sample rate, default `44100`
+    _chunk : nilai chunk , default `1024`
+    _durasi_record : lamanya durasi record audio dalam `second`, default `4 second`
+    
+    Example
+    -------
+    >>> record = create_voice_model(f'C://../../arificial/audio',i++)
+    '''
+    
     MODEL_NAME = input("Masukan nama untuk model voice anda ? ") # kita masukan input nama file diterminal sesuai yang kita mau
     MODEL_WAVE_OUT = f"{MODEL_NAME}_{_model_number}.wav" # output filename audio akan jadi sesuai dengan name yang anda input
     audio = pyaudio.PyAudio() # buka stream
